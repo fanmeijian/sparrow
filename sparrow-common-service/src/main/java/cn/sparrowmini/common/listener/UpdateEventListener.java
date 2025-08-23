f33cb96f0ff5feace096f33ed0952ad511804a90
Sword <fanmeijian@aliyun.com>
2025-08-23 21:46:18 +0800

    fix up update bug

--- sparrow-common-service/src/main/java/cn/sparrowmini/common/listener/UpdateEventListener.java
+++ sparrow-common-service/src/main/java/cn/sparrowmini/common/listener/UpdateEventListener.java
@@ -1,41 +1,41 @@
-package cn.sparrowmini.common.listener;
-
-import org.hibernate.event.spi.PreUpdateEvent;
-import org.hibernate.event.spi.PreUpdateEventListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import cn.sparrowmini.common.CurrentUser;
-import cn.sparrowmini.common.constant.PermissionEnum;
-import cn.sparrowmini.common.model.Model;
-import cn.sparrowmini.common.model.pem.SysroleModel;
-import cn.sparrowmini.common.model.pem.UserModel;
-import cn.sparrowmini.common.service.ModelPermissionService;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Component
-public class UpdateEventListener implements PreUpdateEventListener {
-
-	@Autowired
-	private ModelPermissionService modelPermissionService;
-
-
-	@Override
-	public boolean onPreUpdate(PreUpdateEvent event) {
-		String modelId = event.getEntity().getClass().getName();
-
-		if (modelId.equals(UserModel.class.getName()) || modelId.equals(SysroleModel.class.getName())
-				|| modelId.equals(Model.class.getName())) {
-			return false;
-		}
-
-		if(CurrentUser.get()!=null) {
-			modelPermissionService.hasPermission(modelId, PermissionEnum.EDITOR, CurrentUser.get(),
-					CurrentUser.getUserInfo().getRoles());
-		}
-
-		return false;
-	}
-
-}
+//package cn.sparrowmini.common.listener;
+//
+//import org.hibernate.event.spi.PreUpdateEvent;
+//import org.hibernate.event.spi.PreUpdateEventListener;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import cn.sparrowmini.common.CurrentUser;
+//import cn.sparrowmini.common.constant.PermissionEnum;
+//import cn.sparrowmini.common.model.Model;
+//import cn.sparrowmini.common.model.pem.SysroleModel;
+//import cn.sparrowmini.common.model.pem.UserModel;
+//import cn.sparrowmini.common.service.ModelPermissionService;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//@Component
+//public class UpdateEventListener implements PreUpdateEventListener {
+//
+//	@Autowired
+//	private ModelPermissionService modelPermissionService;
+//
+//
+//	@Override
+//	public boolean onPreUpdate(PreUpdateEvent event) {
+//		String modelId = event.getEntity().getClass().getName();
+//
+//		if (modelId.equals(UserModel.class.getName()) || modelId.equals(SysroleModel.class.getName())
+//				|| modelId.equals(Model.class.getName())) {
+//			return false;
+//		}
+//
+//		if(CurrentUser.get()!=null) {
+//			modelPermissionService.hasPermission(modelId, PermissionEnum.EDITOR, CurrentUser.get(),
+//					CurrentUser.getUserInfo().getRoles());
+//		}
+//
+//		return false;
+//	}
+//
+//}
