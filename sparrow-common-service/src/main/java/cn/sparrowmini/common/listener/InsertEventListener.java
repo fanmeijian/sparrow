f65a019b815b9b804a65608d41bd19a21247b85f
Sword <fanmeijian@aliyun.com>
2025-08-24 14:05:28 +0800

    临时禁用监听器

--- sparrow-common-service/src/main/java/cn/sparrowmini/common/listener/InsertEventListener.java
+++ sparrow-common-service/src/main/java/cn/sparrowmini/common/listener/InsertEventListener.java
@@ -1,39 +1,39 @@
-package cn.sparrowmini.common.listener;
-
-import org.hibernate.event.spi.PreInsertEvent;
-import org.hibernate.event.spi.PreInsertEventListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import cn.sparrowmini.common.CurrentUser;
-import cn.sparrowmini.common.constant.PermissionEnum;
-import cn.sparrowmini.common.model.Model;
-import cn.sparrowmini.common.model.pem.SysroleModel;
-import cn.sparrowmini.common.model.pem.UserModel;
-import cn.sparrowmini.common.service.ModelPermissionService;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Component
-public class InsertEventListener implements PreInsertEventListener {
-
-	@Autowired
-	private ModelPermissionService modelPermissionService;
-
-	@Override
-	public boolean onPreInsert(PreInsertEvent event) {
-		String modelId = event.getEntity().getClass().getName();
-
-		if (modelId.equals(UserModel.class.getName()) || modelId.equals(SysroleModel.class.getName())
-				|| modelId.equals(Model.class.getName())) {
-			return false;
-		}
-		if(CurrentUser.get()!=null) {
-			modelPermissionService.hasPermission(modelId, PermissionEnum.AUTHOR, CurrentUser.get(),
-					CurrentUser.getUserInfo().getRoles());
-		}
-
-		return false;
-	}
-
-}
+//package cn.sparrowmini.common.listener;
+//
+//import org.hibernate.event.spi.PreInsertEvent;
+//import org.hibernate.event.spi.PreInsertEventListener;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import cn.sparrowmini.common.CurrentUser;
+//import cn.sparrowmini.common.constant.PermissionEnum;
+//import cn.sparrowmini.common.model.Model;
+//import cn.sparrowmini.common.model.pem.SysroleModel;
+//import cn.sparrowmini.common.model.pem.UserModel;
+//import cn.sparrowmini.common.service.ModelPermissionService;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//@Component
+//public class InsertEventListener implements PreInsertEventListener {
+//
+//	@Autowired
+//	private ModelPermissionService modelPermissionService;
+//
+//	@Override
+//	public boolean onPreInsert(PreInsertEvent event) {
+//		String modelId = event.getEntity().getClass().getName();
+//
+//		if (modelId.equals(UserModel.class.getName()) || modelId.equals(SysroleModel.class.getName())
+//				|| modelId.equals(Model.class.getName())) {
+//			return false;
+//		}
+//		if(CurrentUser.get()!=null) {
+//			modelPermissionService.hasPermission(modelId, PermissionEnum.AUTHOR, CurrentUser.get(),
+//					CurrentUser.getUserInfo().getRoles());
+//		}
+//
+//		return false;
+//	}
+//
+//}
--- sparrow-common/src/main/java/cn/sparrowmini/common/model/UpsertRepository.java
+++
@@ -1,5 +0,0 @@
-package cn.sparrowmini.common.model;
-public interface UpsertRepository<T, ID> {
-    void upsert(T entity);
-    void upsertAll(Iterable<T> entities);
-}
