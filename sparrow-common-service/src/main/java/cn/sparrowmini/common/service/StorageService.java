8c1c977630eeeceff89ed1df56d52de2464ae27d
Sword <fanmeijian@aliyun.com>
2025-09-01 23:37:13 +0800

    优化依赖和文件

--- sparrow-common-service/src/main/java/cn/sparrowmini/common/service/StorageService.java
+++ sparrow-common-service/src/main/java/cn/sparrowmini/common/service/StorageService.java
@@ -1,11 +1,13 @@
 package cn.sparrowmini.common.service;
 
+import cn.sparrowmini.common.model.ApiResponse;
 import cn.sparrowmini.common.model.BaseFile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Map;
 
 public interface StorageService {
@@ -22,4 +24,6 @@ public interface StorageService {
     public <T extends BaseFile> T getFileInfo(String id);
 
     public <T extends BaseFile> Page<T> getFileList(Pageable pageable, String filter);
+
+    public <T extends BaseFile> ApiResponse<List<T>> createFile(List<Map<String, Object>> fileList);
 }
