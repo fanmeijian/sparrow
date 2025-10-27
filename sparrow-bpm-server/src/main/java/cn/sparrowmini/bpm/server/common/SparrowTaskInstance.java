0a56716b1f8b0a8fbdda611c16c8dcdf05a79f3f
Sword <fanmeijian@aliyun.com>
2025-10-28 03:50:38 +0800

    增加启动时间

--- sparrow-bpm-server/src/main/java/cn/sparrowmini/bpm/server/common/SparrowTaskInstance.java
+++ sparrow-bpm-server/src/main/java/cn/sparrowmini/bpm/server/common/SparrowTaskInstance.java
@@ -1,10 +1,13 @@
 package cn.sparrowmini.bpm.server.common;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+@Data
 public class SparrowTaskInstance implements Serializable {
     private Long id;
     private String title;
@@ -18,121 +21,8 @@ public class SparrowTaskInstance implements Serializable {
     private List<String> potentialOwners;
     private String actualOwner;
     private Date createdOn;
+    private Date activationTime;
     private Map<String,Object> inputData;
     private Map<String,Object> outputData;
 
-    public SparrowTaskInstance() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDeploymentId() {
-        return deploymentId;
-    }
-
-    public void setDeploymentId(String deploymentId) {
-        this.deploymentId = deploymentId;
-    }
-
-    public String getProcessId() {
-        return processId;
-    }
-
-    public void setProcessId(String processId) {
-        this.processId = processId;
-    }
-
-    public Long getProcessInstanceId() {
-        return processInstanceId;
-    }
-
-    public void setProcessInstanceId(Long processInstanceId) {
-        this.processInstanceId = processInstanceId;
-    }
-
-    public String getProcessName() {
-        return processName;
-    }
-
-    public void setProcessName(String processName) {
-        this.processName = processName;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public List<String> getPotentialOwners() {
-        return potentialOwners;
-    }
-
-    public void setPotentialOwners(List<String> potentialOwners) {
-        this.potentialOwners = potentialOwners;
-    }
-
-    public String getActualOwner() {
-        return actualOwner;
-    }
-
-    public void setActualOwner(String actualOwner) {
-        this.actualOwner = actualOwner;
-    }
-
-    public Date getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public Map<String, Object> getInputData() {
-        return inputData;
-    }
-
-    public void setInputData(Map<String, Object> inputData) {
-        this.inputData = inputData;
-    }
-
-    public Map<String, Object> getOutputData() {
-        return outputData;
-    }
-
-    public void setOutputData(Map<String, Object> outputData) {
-        this.outputData = outputData;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
