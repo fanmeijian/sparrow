5336157e94770549a1092176e8d070414f8ddc76
Sword <fanmeijian@aliyun.com>
2026-03-13 11:45:04 +0800

    fix sysrole repository bug

--- sparrow-common-service/src/main/java/cn/sparrowmini/common/repository/SysroleRelationRepository.java
+++ sparrow-common-service/src/main/java/cn/sparrowmini/common/repository/SysroleRelationRepository.java
@@ -3,8 +3,10 @@ package cn.sparrowmini.common.repository;
 import cn.sparrowmini.common.model.pem.SysroleRelation;
 import cn.sparrowmini.common.model.pem.SysroleRelation_;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.repository.NoRepositoryBean;
 
-public interface SysroleRelationRepository<T extends SysroleRelation,ID> extends BaseRepository<T, String> {
+@NoRepositoryBean
+public interface  SysroleRelationRepository<T extends SysroleRelation<?,?>,ID> extends BaseRepository<T, String> {
 
     default boolean existsRelation(ID businessId, String sysroleId) {
         Specification<T> specification = specificationEqual(SysroleRelation_.BUSINESS_ID, businessId)
