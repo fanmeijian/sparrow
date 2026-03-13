cb145efc46f6a7addee7f4a93bfdc8c17cbbeaf8
Sword <fanmeijian@aliyun.com>
2026-03-13 14:16:27 +0800

    优化权限提示

---
+++ sparrow-common-service/src/main/java/cn/sparrowmini/common/exception/BusinessLogicException.java
@@ -0,0 +1,7 @@
+package cn.sparrowmini.common.exception;
+
+public class BusinessLogicException extends RuntimeException{
+    public BusinessLogicException(String message) {
+        super(message);
+    }
+}
--- sparrow-common-service/src/main/java/cn/sparrowmini/common/service/FilePermissionService.java
+++ sparrow-common-service/src/main/java/cn/sparrowmini/common/service/FilePermissionService.java
@@ -1,5 +1,8 @@
 package cn.sparrowmini.common.service;
 
+import cn.sparrowmini.common.exception.BusinessLogicException;
+import cn.sparrowmini.common.exception.DenyPermissionException;
+import cn.sparrowmini.common.exception.NoPermissionException;
 import cn.sparrowmini.common.model.BaseFile;
 
 import java.util.Map;
@@ -8,6 +11,6 @@ import java.util.Map;
  * 文件的下载和上传权限
  */
 public interface FilePermissionService {
-    public boolean canUpload(Map<String, Object> params);
-    public boolean canDownload(BaseFile file, Map<String, Object> params);
+    public boolean canUpload(Map<String, Object> params) throws NoPermissionException, DenyPermissionException, BusinessLogicException;
+    public boolean canDownload(BaseFile file, Map<String, Object> params) throws NoPermissionException, DenyPermissionException, BusinessLogicException;
 }
